d5aab1bc173605c517a115e718f7bb306adb9494
jdeepee <joshuadparkin@gmail.com>
2019-04-19 12:47:35 +0100

    Refactored testing code

--- junto-rust/test/index.js
+++ junto-rust/test/index.js
@@ -1,42 +1,14 @@
-// This test file uses the tape testing framework.
-// To learn more, go here: https://github.com/substack/tape
-const test = require('tape');
+const {Config, Container, Scenario} = require("@holochain/holochain-nodejs")
+Scenario.setTape(require('tape'))
 
-const { Config, Container } = require("@holochain/holochain-nodejs")
+const dnaPath = "./dist/junto-rust.dna.json"
 
-const dnaPath = "./dist/bundle.json"
+const dna = Config.dna(dnaPath)
+const agentJosh = Config.agent("josh")
+const instanceJosh = Config.instance(agentJosh, dna)
+const scenario = new Scenario([instanceJosh], {debugLog: true})
 
-// closure to keep config-only stuff out of test scope
-const container = (() => {
-    const agentJosh = Config.agent("josh")
-
-    const dna = Config.dna(dnaPath)
-
-    const instanceJosh = Config.instance(agentJosh, dna)
-
-    const containerConfig = Config.container([instanceJosh])
-    return new Container(containerConfig)
-})()
-
-// Initialize the Container
-container.start()
-
-const app = container.makeCaller('josh', dnaPath)
-
-test('Creating user test', (t) => {
-  // Make a call to a Zome function
-  // indicating the capability and function, and passing it an input
-    let user = app.call("core", "main", "create_user", {user_data : {
-      parent: "hashstring", //Parent HashString data objects to be contextual to given data trees
-      first_name: "Test user",
-      last_name: "Test Last",
-      bio: "Bio test user",
-      profile_picture: "picture test user",
-      verified: true}})
-
-  // check for equality of the actual and expected results
-  t.deepEqual(user, { Ok: { App: [ 'my_entry', '{"content":"sample content"}' ] } })
-
-  // ends this test
-  t.end()
+scenario.runTape('Can register profile and retrieve', async (t, {josh}) =>{
+  const register_result = await josh.callSync("core", "create_user", {user_data: {first_name: "Josh", last_name: "Parkin", bio: "Junto Testing", profile_picture: "pictureurl"}})
+  console.log(register_result)
 })
