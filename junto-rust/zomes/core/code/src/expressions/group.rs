3910e228bdad19ea4a2cbc049147663898b03e90
jdeepee <joshuadparkin@gmail.com>
2019-05-24 12:42:31 +0100

    Converted groups to type/tag functionality

--- junto-rust/zomes/core/code/src/expressions/group.rs
+++ junto-rust/zomes/core/code/src/expressions/group.rs
@@ -37,10 +37,9 @@ pub fn create_pack(username_address: &Address, first_name: String) -> ZomeApiRes
     };
     let entry = Entry::App("group".into(), pack.clone().into());
     let address = hdk::commit_entry(&entry)?;
-    let hook_definitions = vec![FunctionDescriptor{name: "time_to_expression", parameters: FunctionParameters::TimeToExpression{tag: "group", direction: "forward", expression_address: address.clone(), context: Address::from(DNA_ADDRESS.to_string())}},
-                                FunctionDescriptor{name: "time_to_expression", parameters: FunctionParameters::TimeToExpression{tag: "pack", direction: "forward", expression_address: address.clone(), context: Address::from(DNA_ADDRESS.to_string())}},
-                                FunctionDescriptor{name: "link_expression", parameters: FunctionParameters::LinkExpression{tag: "pack", direction: "reverse", parent_expression: address.clone(), child_expression: username_address.clone()}},
-                                FunctionDescriptor{name: "link_expression", parameters: FunctionParameters::LinkExpression{tag: "owner", direction: "forward", parent_expression: address.clone(), child_expression: username_address.clone()}}];
+    let hook_definitions = vec![FunctionDescriptor{name: "time_to_expression", parameters: FunctionParameters::TimeToExpression{link_type: "group", tag: "pack", direction: "forward", expression_address: address.clone(), context: Address::from(DNA_ADDRESS.to_string())}},
+                                FunctionDescriptor{name: "link_expression", parameters: FunctionParameters::LinkExpression{link_type: "group", tag: "pack", direction: "reverse", parent_expression: address.clone(), child_expression: username_address.clone()}},
+                                FunctionDescriptor{name: "link_expression", parameters: FunctionParameters::LinkExpression{link_type: "auth", tag: "owner", direction: "forward", parent_expression: address.clone(), child_expression: username_address.clone()}}];
 
     let _hook_result = utils::handle_hooks("Group".to_string(), hook_definitions)?;
     channel::create_collective_channel(&address)?;
@@ -59,7 +58,7 @@ pub fn add_member_to_group(username_address: Address, group: Address) -> ZomeApi
     if group_owner == true{
         let group_member = is_group_member(group.clone(), username_address.clone())?;
         if group_member == false{
-            hdk::api::link_entries(&group, &username_address, "member".to_string())?;
+            hdk::api::link_entries(&group, &username_address, "auth".to_string(), "member".to_string())?;
             Ok(json!({ "message": "User added to group" }).into())
         } else {
             Err(ZomeApiError::from("User submitted is already a member of given group".to_string()))
@@ -75,7 +74,7 @@ pub fn remove_group_member(username_address: Address, group: Address) -> ZomeApi
     if group_owner == true{
         let group_member = is_group_member(group.clone(), username_address.clone())?;
         if group_member == true{
-            hdk::api::remove_link(&group, &username_address, "member".to_string())?;
+            hdk::api::remove_link(&group, &username_address, "auth".to_string(), "member".to_string())?;
             Ok(json!({ "message": "User removed from group" }).into())
         } else {
             Err(ZomeApiError::from("User submitted is not a group member of given group".to_string()))
@@ -90,7 +89,7 @@ pub fn is_group_member(group: Address, user: Address) -> ZomeApiResult<bool>{
     match group_entry {
         Some(Entry::App(_, entry_value)) => {
             let _entry = app_definitions::Group::try_from(&entry_value).map_err(|_err| ZomeApiError::from("Specified group address is not of type Group".to_string()))?; //will return error here if cannot ser entry to group
-            let member_vec = utils::get_links_and_load_type::<String, app_definitions::UserName>(&group, "member".to_string())?;
+            let member_vec = utils::get_links_and_load_type::<app_definitions::UserName>(&group, Some("auth".to_string()), Some("member".to_string()))?;
             for member in member_vec {
                 if member.address == user{
                     return Ok(true)
@@ -113,7 +112,7 @@ pub fn get_group_members(group: Address) -> ZomeApiResult<GroupMembers> {
             if is_group_owner(group.clone(), current_user_username.clone())? == false && is_group_member(group.clone(), current_user_username.clone())? == false {
                 return Err(ZomeApiError::from("You are not an owner or member of this group and thus are not allowed to view given information".to_string()))
             };
-            let member_vec = utils::get_links_and_load_type::<String, app_definitions::UserName>(&group, "member".to_string())?;
+            let member_vec = utils::get_links_and_load_type::<app_definitions::UserName>(&group, Some("auth".to_string()), Some("member".to_string()))?;
             Ok(GroupMembers{members: member_vec})
         },
         Some(_) => Err(ZomeApiError::from("Context address was not an app entry".to_string())),
