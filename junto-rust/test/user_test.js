96a4a8dd47129cbb1b0df31b3bb7139e45f75972
jdeepee <joshuadparkin@gmail.com>
2019-05-29 20:48:08 +0100

    Changed tests to reflect new function outputs as well as removed old functions

--- junto-rust/test/user_test.js
+++ junto-rust/test/user_test.js
@@ -22,38 +22,16 @@ scenario.runTape('Can register a profile and retrieve', async (t, {josh}) => {
 
   const get_user_profile_from_address = await josh.callSync('core', 'get_user_profile_from_address', {username_address: 'QmT7TDNsrKw2psyvYJztAMVFyKowPtR5VLbwDVHbtuoWSn'})
   console.log("Getting user profile by address", get_user_profile_from_address) //should return profile
-  t.equal(JSON.stringify(get_user_profile_from_address), JSON.stringify({ Ok: 
-    { parent: 'QmT7TDNsrKw2psyvYJztAMVFyKowPtR5VLbwDVHbtuoWSn',
-      first_name: 'Josh',
-      last_name: 'Parkin',
-      bio: 'Junto Testing',
-      profile_picture: 'pictureurl',
-      verified: true } }))
+  t.equal(JSON.stringify(get_user_profile_from_address), JSON.stringify({ Ok: {"address": "QmQ2UTpz5EGD3v5N5iZe6FwaGWgbFGazATTC2RQvB5SuR1", "entry": {"parent":"QmT7TDNsrKw2psyvYJztAMVFyKowPtR5VLbwDVHbtuoWSn","first_name":"Josh","last_name":"Parkin","bio":"Junto Testing","profile_picture":"pictureurl","verified":true}} }))
   console.log("Completed get profile by address\n\n\n")
 
   const get_user_profile_by_agent_address = await josh.callSync('core', 'get_user_profile_by_agent_address', {})
   console.log("Get user profile", get_user_profile_by_agent_address) //should return profile
-  t.equal(JSON.stringify(get_user_profile_by_agent_address), JSON.stringify({ Ok: 
-    { parent: 'QmT7TDNsrKw2psyvYJztAMVFyKowPtR5VLbwDVHbtuoWSn',
-      first_name: 'Josh',
-      last_name: 'Parkin',
-      bio: 'Junto Testing',
-      profile_picture: 'pictureurl',
-      verified: true } }))
+  t.equal(JSON.stringify(get_user_profile_by_agent_address), JSON.stringify({ Ok: {"address": "QmQ2UTpz5EGD3v5N5iZe6FwaGWgbFGazATTC2RQvB5SuR1", "entry": {"parent":"QmT7TDNsrKw2psyvYJztAMVFyKowPtR5VLbwDVHbtuoWSn","first_name":"Josh","last_name":"Parkin","bio":"Junto Testing","profile_picture":"pictureurl","verified":true}} }))
   console.log("Completed get profile by agent address\n\n\n")
-
-  const get_user_profile_address_by_agent_address = await josh.callSync('core', 'get_user_profile_address_by_agent_address', {})
-  console.log("Get user profile address", get_user_profile_address_by_agent_address) //should return address
-  t.equal(JSON.stringify(get_user_profile_address_by_agent_address), JSON.stringify({ Ok: 'QmQ2UTpz5EGD3v5N5iZe6FwaGWgbFGazATTC2RQvB5SuR1' }))
-  console.log("Completed get profile address by agent address\n\n\n")
 
   const get_user_username_by_agent_address = await josh.callSync('core', 'get_user_username_by_agent_address', {})
   console.log("Get user username", get_user_username_by_agent_address) //should return username
-  t.equal(JSON.stringify(get_user_username_by_agent_address), JSON.stringify({ Ok: { username: 'jdeepee' } }))
+  t.equal(JSON.stringify(get_user_username_by_agent_address), JSON.stringify({ Ok: {"address": "QmT7TDNsrKw2psyvYJztAMVFyKowPtR5VLbwDVHbtuoWSn", "entry": {"username":"jdeepee"}} }))
   console.log("Completed get username by agent address\n\n\n")
-
-  const get_user_username_address_by_agent_address = await josh.callSync('core', 'get_user_username_address_by_agent_address', {})
-  console.log("Get username address", get_user_username_address_by_agent_address) //should return username address 
-  t.equal(JSON.stringify(get_user_username_address_by_agent_address), JSON.stringify({ Ok: 'QmT7TDNsrKw2psyvYJztAMVFyKowPtR5VLbwDVHbtuoWSn' }))
-  console.log("Completed get username address by agent address\n\n\n")
 })
