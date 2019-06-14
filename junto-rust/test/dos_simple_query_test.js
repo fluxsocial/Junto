a82341d79a9409117e51a698c93615edf77a8b0d
jdeepee <joshuadparkin@gmail.com>
2019-06-14 11:39:54 +0100

    Updated tests

--- junto-rust/test/dos_simple_query_test.js
+++ junto-rust/test/dos_simple_query_test.js
@@ -9,9 +9,9 @@ const agentJosh = Config.agent("josh")
 const agentEric = Config.agent("eric")
 const instanceJosh = Config.instance(agentJosh, dna)
 const instanceEric = Config.instance(agentEric, dna)
-const scenario = new Scenario([instanceJosh, instanceEric], {debugLog: true}) 
+const scenario = new Scenario([instanceJosh, instanceEric], {debugLog: false}) 
 
-scenario.runTape('Can register a profile and retrieve', async (t, {josh, eric}) => {
+scenario.runTape('Can post expression and do basic DOS query', async (t, {josh, eric}) => {
     const register_result = await josh.callSync('core', 'create_user', {user_data: {username: "jdeepee", first_name: "Josh", last_name: "Parkin", bio: "Junto Testing", profile_picture: "pictureurl"}});
     console.log("Register user result", register_result);
     t.equal(JSON.stringify(register_result), JSON.stringify({"Ok":{"private_den":{"address":"QmV7H3Mhpdpj9NfFq2pgwzRd83uEjQupsHa5zwVVeCWSd2","entry":{"parent":"QmT7TDNsrKw2psyvYJztAMVFyKowPtR5VLbwDVHbtuoWSn","name":"Josh\'s Den","privacy":"Private","channel_type":"Den"}},"shared_den":{"address":"QmV9j9LNfc4spvT8qNA24vjMjC4JEnoVfidfiBfnY4PUs3","entry":{"parent":"QmT7TDNsrKw2psyvYJztAMVFyKowPtR5VLbwDVHbtuoWSn","name":"Josh\'s Den","privacy":"Shared","channel_type":"Den"}},"public_den":{"address":"Qmc48qWCdrCEqJVn1a4XZd6Eyrsu1W5jHHi1CgsVJgEAMx","entry":{"parent":"QmT7TDNsrKw2psyvYJztAMVFyKowPtR5VLbwDVHbtuoWSn","name":"Josh\'s Den","privacy":"Public","channel_type":"Den"}},"pack":{"address":"QmW8j2NrAvKzUTQxtYnPGXmz7PgRevrGgvkD21jHC2utHA","entry":{"parent":"QmT7TDNsrKw2psyvYJztAMVFyKowPtR5VLbwDVHbtuoWSn","name":"Josh\'s Pack","owner":"QmT7TDNsrKw2psyvYJztAMVFyKowPtR5VLbwDVHbtuoWSn","privacy":"Shared"}},"profile":{"address":"QmQ2UTpz5EGD3v5N5iZe6FwaGWgbFGazATTC2RQvB5SuR1","entry":{"parent":"QmT7TDNsrKw2psyvYJztAMVFyKowPtR5VLbwDVHbtuoWSn","first_name":"Josh","last_name":"Parkin","bio":"Junto Testing","profile_picture":"pictureurl","verified":true}},"username":{"address":"QmT7TDNsrKw2psyvYJztAMVFyKowPtR5VLbwDVHbtuoWSn","entry":{"username":"jdeepee"}},"user_perspective":{"address":"QmcBgVN5mo8ACrX1Z1f2ZXNFzbRWSGhMskuNoJXe9fYQ71","entry":{"parent":"QmT7TDNsrKw2psyvYJztAMVFyKowPtR5VLbwDVHbtuoWSn","name":"Default Perspective","privacy":"Private","channel_type":"Perspective"}}}}));
@@ -67,7 +67,6 @@ scenario.runTape('Can register a profile and retrieve', async (t, {josh, eric}) 
     t.equal(JSON.stringify(post_global_expression), JSON.stringify({ Ok: 'QmZ23wNYx8BNtHMcG6kYNufycR6s8dXyqWP6ySYsTbHnPg' }));
     console.log("Completed posting expression\n\n\n\n");
 
-    //This query should use the current data/time for time query points
     let d = new Date();
     let year = d.getFullYear();
     let month = d.getUTCMonth() + 1;
@@ -83,4 +82,4 @@ scenario.runTape('Can register a profile and retrieve', async (t, {josh, eric}) 
     console.log("Make 1 dos query result", make_1_dos_query);
     t.equal(JSON.stringify(make_1_dos_query), JSON.stringify({"Ok":[{"address":"QmZ23wNYx8BNtHMcG6kYNufycR6s8dXyqWP6ySYsTbHnPg","entry":{"expression_type":"PostExpression","expression":{"PostExpression":{"post":"This is the first test expression"}}}}]}));
     console.log("Completed ")
-})
+});
--- junto-rust/test/user_test.js
+++ junto-rust/test/user_test.js
@@ -7,7 +7,7 @@ const dnaPath = "./dist/junto-rust.dna.json"
 const dna = Config.dna(dnaPath)
 const agentJosh = Config.agent("josh")
 const instanceJosh = Config.instance(agentJosh, dna)
-const scenario = new Scenario([instanceJosh], {debugLog: true}) 
+const scenario = new Scenario([instanceJosh], {debugLog: false}) 
 
 scenario.runTape('Can register a profile and retrieve', async (t, {josh}) => {
   const register_result = await josh.callSync('core', 'create_user', {user_data: {username: "jdeepee", first_name: "Josh", last_name: "Parkin", bio: "Junto Testing", profile_picture: "pictureurl"}})
