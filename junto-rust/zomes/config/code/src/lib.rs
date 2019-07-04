e2936074a23a6acf9f4a316358c457d98124849a
jdeepee <joshuadparkin@gmail.com>
2019-07-04 13:32:44 +0800

    Added config zome for bucket and config entries

---
+++ junto-rust/zomes/config/code/src/lib.rs
@@ -0,0 +1,21 @@
+#![feature(try_from)]
+#[macro_use]
+extern crate hdk;
+extern crate serde;
+extern crate serde_derive;
+extern crate serde_json;
+extern crate holochain_core_types_derive;
+extern crate types;
+extern crate utils;
+
+define_zome! {
+    entries: []
+
+    genesis: || { Ok(()) }
+
+    functions: []
+
+    traits: {
+        hc_public []
+    }
+}
