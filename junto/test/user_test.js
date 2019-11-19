1a3b57e6d2d328b00527cc73f2f64d7d607491fc
jdeepee <joshuadparkin@gmail.com>
2019-11-19 17:05:58 +0000

    now uses tryorama & public lib2h ws for testing

--- junto/test/user_test.js
+++ junto/test/user_test.js
@@ -1,37 +1,55 @@
-const {Diorama, tapeExecutor} = require('@holochain/diorama')
-const scenarios = require("./scenarios.js")
-const dnaPath = Diorama.dna('./dist/junto-rust.dna.json', 'junto')
+const { Orchestrator, Config } = require('@holochain/tryorama');
+const scenarios = require("./scenarios.js");
 
-const diorama = new Diorama({
-    instances: {
-        agent1: dnaPath
+const dnaJunto = Config.dna('./dist/junto.dna.json', 'junto');
+
+const mainConfig = Config.gen(
+    {
+      junto: dnaJunto,  // agent_id="blog", instance_id="blog", dna=dnaBlog
     },
-    debugLog: false,
-    executor: tapeExecutor(require('tape'))
-});
+    {
+      // specify a bridges
+      bridges: [],
+      // use a sim2h network
+      network: {
+        type: 'sim2h',
+        sim2h_url: 'wss://sim2h.holochain.org:9000',
+      },
+      // etc., any other valid conductor config items can go here
+    }
+);
+  
+const orchestrator = new Orchestrator();
 
-diorama.registerScenario('Can register a profile and retrieve', async (s, t, {agent1}) => {
-    const user1 = await scenarios.registerAgent(t, agent1, "jdeepee", "joshua", "parkin").catch(err => { console.log(err) } );
+orchestrator.registerScenario
 
-    const get_username_from_address = await agent1.call('user', 'get_username_from_address', {username_address: user1.Ok.username.address})
+// Run all registered scenarios as a final step, and gather the report,
+// if you set up a reporter
+orchestrator.registerScenario('Can register a profile and retrieve', async (s, t) => {
+    const {user1} = await s.players({user1: mainConfig}, true);
+    let user1_res = await scenarios.registerAgent(t, user1, "jdeepee", "joshua", "parkin").catch(err => { console.log(err) } );
+    await s.consistency();
+
+    const get_username_from_address = await user1.call('junto', 'user', 'get_username_from_address', {username_address: user1_res.Ok.username.address})
     console.log("Getting user by address", get_username_from_address) //should return username
     t.equal(JSON.stringify(get_username_from_address), JSON.stringify({ Ok: { username: 'jdeepee' } }))
     console.log("Completed get username by address\n\n\n")
-  
-    const get_user_profile_from_address = await agent1.call('user', 'get_user_profile_from_address', {username_address: user1.Ok.username.address})
+
+    const get_user_profile_from_address = await user1.call('junto', 'user', 'get_user_profile_from_address', {username_address: user1_res.Ok.username.address})
     console.log("Getting user profile by address", get_user_profile_from_address) //should return profile
-    t.equal(JSON.stringify(get_user_profile_from_address), JSON.stringify({Ok: user1.Ok.profile}))
+    t.equal(JSON.stringify(get_user_profile_from_address), JSON.stringify({Ok: user1_res.Ok.profile}))
     console.log("Completed get profile by address\n\n\n")
-  
-    const get_user_profile_by_agent_address = await agent1.call('user', 'get_user_profile_by_agent_address', {})
+
+    const get_user_profile_by_agent_address = await user1.call('junto', 'user', 'get_user_profile_by_agent_address', {})
     console.log("Get user profile", get_user_profile_by_agent_address) //should return profile
-    t.equal(JSON.stringify(get_user_profile_by_agent_address), JSON.stringify({Ok: user1.Ok.profile}));
+    t.equal(JSON.stringify(get_user_profile_by_agent_address), JSON.stringify({Ok: user1_res.Ok.profile}));
     console.log("Completed get profile by agent address\n\n\n")
   
-    const get_user_username_by_agent_address = await agent1.call('user', 'get_user_username_by_agent_address', {})
+    const get_user_username_by_agent_address = await user1.call('junto', 'user', 'get_user_username_by_agent_address', {})
     console.log("Get user username", get_user_username_by_agent_address) //should return username
-    t.equal(JSON.stringify(get_user_username_by_agent_address), JSON.stringify({Ok: user1.Ok.username}));
+    t.equal(JSON.stringify(get_user_username_by_agent_address), JSON.stringify({Ok: user1_res.Ok.username}));
     console.log("Completed get username by agent address\n\n\n")
 });
 
-diorama.run();
+const report = orchestrator.run()
+console.log(report)
