9bb301b7c13216d283930ba4ca847d92c4e4361b
Adam <a_martin15@hotmail.com>
2020-01-26 20:47:15 +0000

    added getExpression Http method

---
+++ ui-src/src/components/ExpressionViewer/ExpressionViewerHttp.js
@@ -0,0 +1,50 @@
+import { isSuccess, makeHolochainCallAndReturn } from "./../../utils";
+
+function getExpression(
+  template,
+  perspective,
+  attributes,
+  queryOptions,
+  targetType,
+  queryType,
+  dos,
+  seed
+) {
+  return makeHolochainCallAndReturn(
+    template.$store.getters.getHolochainConnection,
+    "expression",
+    "query_expressions",
+    {
+      perspective: perspective,
+      attributes: attributes,
+      query_options: queryOptions,
+      target_type: targetType,
+      query_type: queryType,
+      dos: dos,
+      seed: seed
+    }
+  )
+    .then(result => {
+      result = JSON.parse(result);
+      if (isSuccess(result) == true) {
+        console.log("Great Success getting expressions", result);
+      } else {
+        console.log("Error getting expressions: ", result);
+        template.$notify({
+          type: "error",
+          group: "main",
+          title:
+            "There was an error retrieving this posting expression. Error is: ",
+          text: result.Err.Internal,
+          duration: 5000
+        });
+      }
+    })
+    .catch(err => {
+      console.log("getExpression Failed: ", err);
+    });
+}
+
+export default {
+  getExpression
+};
---
+++ ui-src/src/components/ExpressionViewer/Shortform/Shortform.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="shortform-view">
+
+  </section>
+</template>
+
+<script>
+import expressionViewerHttpMethods from "./../ExpressionViewerHttp";
+
+export default {
+  name: "Shortform",
+  data() {
+    return {
+      perspective: String,
+      attributes: Array,
+      queryOptions: String,
+      targetType: String,
+      queryType: String,
+      dos: Number,
+      seed: String
+    };
+  },
+  methods: {
+    getExpression() {
+      expressionViewerHttpMethods.getExpression(
+        this.perspective,
+        this.attributes,
+        this.queryOptions,
+        this.targetType,
+        this.queryType,
+        this.dos,
+        this.seed
+      );
+    }
+  }
+};
+</script>
